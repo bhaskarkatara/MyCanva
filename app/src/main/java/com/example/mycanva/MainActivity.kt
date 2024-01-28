0499ed3bbfca9e4e20edccf7e46fd46c2016a2da
bhaskar katara <bhaskarkatararjo5@gmail.com>
2024-01-28 20:11:58 +0530

    Add Image access from user gallery & provide an Undo Button

--- app/src/main/java/com/example/mycanva/MainActivity.kt
+++ app/src/main/java/com/example/mycanva/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.mycanva
 
 import android.Manifest
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -19,6 +22,48 @@ class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? =
         null // A variable for current color is picked from color pallet.
+
+    val openGalleryLauncher:ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
+
+        if (result.resultCode == RESULT_OK && result.data != null){
+
+            val imageBackground:ImageView = findViewById(R.id.iv_background)
+            imageBackground.setImageURI(result.data?.data)
+        }
+    }
+
+    /** create an ActivityResultLauncher with MultiplePermissions since we are requesting
+     * both read and write
+     */
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val perMissionName = it.key
+                val isGranted = it.value
+                //if permission is granted show a toast and perform operation
+                if (isGranted ) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Permission granted now you can read the storage files.",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    val pickIntent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+                } else {
+
+                    if (perMissionName == Manifest.permission.READ_EXTERNAL_STORAGE)
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Oops you just denied the permission.",
+                            Toast.LENGTH_LONG
+                        ).show()
+                }
+            }
+
+        }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +81,16 @@ class MainActivity : AppCompatActivity() {
         )
         ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
+        }
+        val ibGallery: ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener {
+            requestStoragePermission()
+        }
+        val ibUndo: ImageButton = findViewById(R.id.ib_undo)
+        // TODO(Step 4 - Adding an click event for undo option.)
+        ibUndo.setOnClickListener {
+            // This is for undo recent stroke.
+            drawingView?.onClickUndo()
         }
     }
 
@@ -65,7 +120,6 @@ class MainActivity : AppCompatActivity() {
         brushDialog.show()
     }
 
-    // TODO(Step 2 - A function for color selection.)
     /**
      * Method is called when color is clicked from pallet_normal.
      *
@@ -75,7 +129,7 @@ class MainActivity : AppCompatActivity() {
         if (view !== mImageButtonCurrentPaint) {
             // Update the color
             val imageButton = view as ImageButton
-            // Here the tag is used for swapping the current color with previous color.
+            // Here the tag is used for swaping the current color with previous color.
             // The tag stores the selected view
             val colorTag = imageButton.tag.toString()
             // The color is set as per the selected tag here.
@@ -93,5 +147,47 @@ class MainActivity : AppCompatActivity() {
             mImageButtonCurrentPaint = view
         }
     }
-}
 
+    //create a method to requestStorage permission
+    private fun requestStoragePermission(){
+        // Check if the permission was denied and show rationale
+        if (
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+        ){
+            //call the rationale dialog to tell the user why they need to allow permission request
+            showRationaleDialog("Kids Drawing App","Kids Drawing App " +
+                    "needs to Access Your External Storage")
+        }
+        else {
+            // You can directly ask for the permission.
+            //if it has not been denied then request for permission
+            //  The registered ActivityResultCallback gets the result of this request.
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
+        }
+
+    }
+    /**  create rationale dialog
+     * Shows rationale dialog for displaying why the app needs permission
+     * Only shown if the user has denied the permission request previously
+     */
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
+}
